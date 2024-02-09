a6063d693a61ede86a562b7342e2d8657d1f69c8
sivasoma <sivanaidusoma@gmail.com>
2024-02-09 16:28:39 +0530

    initial commit

---
+++ prompt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#please enter your user name
+
+echo "please enter your user name:"
+read USERNAME # this is the variable where the value of the user
+echo "please enter your user name: $USERNAME"
+echo "please enter your Password:"
+read -s PASSWORD
+echo "please enter your Password:$Password"
