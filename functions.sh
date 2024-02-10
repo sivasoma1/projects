e589ff675c95bd0303c2ec6dfc4c8d095bdde566
sivasoma <sivanaidusoma@gmail.com>
2024-02-10 16:41:45 +0530

    initial commit

--- functions.sh
+++ functions.sh
@@ -3,24 +3,30 @@
 #functions will do some work behalf of us, it updates
 #we need to give inputs,function will give output
 userid=$(id -u)
-validate (){
+
+VALIDATE(){
+
     if [ $1 -ne 0]
     then 
-        echo "the statement is error :please check the given command"
+        echo "the statement is error :please check the given command "
     else
-        echo "installation is success $@"
+        echo "installation is success  "
 }
 
 if [ $userid -ne 0 ]
 then 
     echo "You need to run this script as root."
     exit 1 #1-127 anything is error(exit status), 0 means success.
+    
 fi
-validate $?
+
 yum install mysql -y
 
-validate $?
-yum install mysql -y
+VALIDATE $?
+
+yum install postfix -y
+
+VALIDATE $?
 
 
 
