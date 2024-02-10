20d53cb27104af97c0d74c08c98d7b627d2c14ca
sivasoma <sivanaidusoma@gmail.com>
2024-02-10 16:36:48 +0530

    initial commit

--- conditions2.sh
+++ conditions2.sh
@@ -14,3 +14,11 @@ else
 	echo "Failed to install MySQL"
 	exit 1
 fi
+yum install postfix -y
+
+if [ $? -eq 0 ]; then
+        echo "MySQL has been installed successfully!"
+else
+	echo "Failed to install postfix"
+	exit 1
+fi
