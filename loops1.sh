81b369966146c4909406c78e68eab5714ee0a2a6
siva <sivasoma1112@gmail.com>
2024-02-12 15:50:42 +0530

    initial commit

--- loops1.sh
+++ loops1.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+userid=$(id -u)
+date=$(date +%F)
+logfile=/tmp/$filename-$date.log
+R="\e[31m" #red colour
+G="\e[32m" #green colour
+Y="\e[33m" #yellow colour
+N="\[0m" # No Color
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+
+    then
+        echo "$2"
+
+}
  for i in $@
  do 
     yum install $i -y
