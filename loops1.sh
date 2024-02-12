caf89eca05a884d4f5484483b34b697f25bc4283
siva <sivasoma1112@gmail.com>
2024-02-12 16:06:39 +0530

    initial commit

--- loops1.sh
+++ loops1.sh
@@ -37,6 +37,6 @@ VALIDATE(){
         
         VALIDATE $? "Installing package $i"
     else
-        echo "$y it is already installed $i"
+        echo -e "$y it is already installed $i"
     fi
 done
