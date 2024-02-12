7788872a1a9c902b4a6592036397af5eb3439699
siva <sivasoma1112@gmail.com>
2024-02-12 16:02:52 +0530

    initial commit

--- loops1.sh
+++ loops1.sh
@@ -7,14 +7,35 @@ G="\e[32m" #green colour
 Y="\e[33m" #yellow colour
 N="\[0m" # No Color
 
+if [  $userid -ne 0 ]
+then
+    echo " $R error: you must run this script as root user.$N"
+    exit 1
+fi
+
 VALIDATE(){
     if [ $1 -ne 0 ]
 
     then
-        echo "$2"
+        echo "$2 is $R failure $N"
+        exit 1
+    else
+        echo "$2 is $G success $N"
+    fi
 
 }
+
+
+
  for i in $@
  do 
-    yum install $i -y
+    yum list installed $i &>> $logfile
+    if [ $? -ne 0 ]
+    then
+        yum install $i -y &>> $logfile
+        
+        VALIDATE $? "Installing package $i"
+    else
+        "$y it is already installed $i"
+    fi
 done
