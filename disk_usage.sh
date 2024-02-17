ad732ea6eccdd360bb1ef305d93b9c23859fd2c4
siva <sivasoma1112@gmail.com>
2024-02-17 17:38:42 +0530

    initial commit

--- disk_usage.sh
+++ disk_usage.sh
@@ -6,7 +6,7 @@ log_file=$log_file_dir/$Filename-$Date.log
 
 Disk_Usage=$(df -hT | grep -vE 'tmpfs|Filesystem')
 Disk_Threshold=1
- 
+message=""
 #IFS means internal field separator
 
 while IFS= read line
@@ -15,11 +15,11 @@ do
     Usage=$(echo $line | awk '{print $7}' | cut -d % -f1)
     
    #Checking if the usage is greater than threshold, then send an email 
-    partion=$(echo $line | awk '{print $1}')
+    partition=$(echo $line | awk '{print $1}')
 
     if [ $Usage -ge $Disk_Threshold ]; then
-        echo "WARNING: Disk usage is over threshold of $Disk_Threshold%. Current
-        exit 1
+        message+= "WARNING: $partition has reached its limit. $Usage \n"
+        
 
     fi
     
