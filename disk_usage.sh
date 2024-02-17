61ef4c17e2bf41d56ebcc837313a480db12aae28
siva <sivasoma1112@gmail.com>
2024-02-17 17:18:51 +0530

    initial commit

--- disk_usage.sh
+++ disk_usage.sh
@@ -4,7 +4,7 @@ Date=$(date +%F:%H:%M:%S)
 Filename=$0
 log_file=$log_file_dir/$Filename-$Date.log
 
-Disk_Usage=$(df -hT |grep -vE 'tmpfs | Filesystem')
+Disk_Usage=$(df -hT | grep -vE 'tmpfs|Filesystem')
 Disk_Threshold=1
  
 #IFS means internal field separator
