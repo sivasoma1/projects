82ef95db3017df582cb5a74602a10721549ba921
sivasoma <sivanaidusoma@gmail.com>
2024-02-23 16:29:07 +0530

    initial commit

--- catalogue.sh
+++ catalogue.sh
@@ -9,6 +9,15 @@ LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ];
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+# else
+#     echo "INFO:: You are root user"
+fi
 
 # this function should validate the previous command and inform user it is success or failure
 VALIDATE(){
@@ -22,15 +31,7 @@ VALIDATE(){
     fi
 }
 
-USERID=$(id -u)
 
-if [ $USERID -ne 0 ];
-then
-    echo "ERROR:: Please run this script with root access"
-    exit 1
-# else
-#     echo "INFO:: You are root user"
-fi
 
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
