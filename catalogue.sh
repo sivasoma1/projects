0a927c34b3302e7b3acad1bce9f25c3ea5f4899a
sivasoma <sivanaidusoma@gmail.com>
2024-02-23 17:14:09 +0530

    initial commit

--- catalogue.sh
+++ catalogue.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
-# our program goal is to install mysql
-
 DATE=$(date +%F)
+LOGSDIR=/home/centos/shellscript-logs
+# /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
-LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
-
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-USERID=$(id -u)
+Y="\e[33m"
 
 if [ $USERID -ne 0 ];
 then
-    echo "ERROR:: Please run this script with root access"
+    echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
-# else
-#     echo "INFO:: You are root user"
 fi
 
-# this function should validate the previous command and inform user it is success or failure
 VALIDATE(){
-    #$1 --> it will receive the argument1
     if [ $1 -ne 0 ];
     then
-        echo -e "$2 ... $R FAILURE $N"
+        echo -e "Installing $2 ... $R FAILURE $N"
         exit 1
     else
-        echo -e "$2 ... $G SUCCESS $N"
+        echo -e "Installing $2 ... $G SUCCESS $N"
     fi
 }
 
