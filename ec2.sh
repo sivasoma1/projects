b1770baeb7a04b45781837537602b08c9f9299a5
sivasoma <sivanaidusoma@gmail.com>
2024-03-04 17:37:52 +0530

    initial commit

--- ec2.sh
+++ ec2.sh
@@ -5,8 +5,9 @@ INSTANCE_TYPE=""
 SECURITY_GROUP_ID=sg-012b40d35bddf900e
 DOMAIN_NAME=ssshankar.site
 IMAGE_ID=ami-0f3c7d07486cad139
+HOSTED_ZONE_ID=Z07242593H082AXGG73OV
 
-
+# Loop through the instances
 for i in "${NAME[@]}"; do
     # Check if the instance already exists
     existing_instance=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$i" --query "Reservations[*].Instances[*].InstanceId" --output text)
@@ -15,33 +16,28 @@ for i in "${NAME[@]}"; do
         continue
     fi
 
-# mysql and mongodb are in t3.medium, others are in t2.micro
-for i in "${NAME[@]}";
-do
-    if [ $i == "mysql" ] || [ $i == "mongodb" ]; 
-    then
-        
+    # Determine instance type
+    if [ "$i" == "mysql" ] || [ "$i" == "mongodb" ]; then
         INSTANCE_TYPE="t3.medium"
-
     else
         INSTANCE_TYPE="t2.micro"
     fi
 
+    # Create EC2 instance
+    echo "Creating $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "Created $i instance: $IP_ADDRESS"
 
-    echo "creating $i instance"
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-    echo "created $i instance: $IP_ADDRESS"
-
-    aws route53 change-resource-record-sets --hosted-zone-id Z07242593H082AXGG73OV --change-batch '
-    {
-            "Changes": [{
+    # Update or create Route 53 record
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '{
+        "Changes": [{
             "Action": "UPSERT",
-                        "ResourceRecordSet": {
-                            "Name": "'$i.$DOMAIN_NAME'",
-                            "Type": "A",
-                            "TTL": 300,
-                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
-                        }}]
-    }
-    '
+            "ResourceRecordSet": {
+                "Name": "'$i.$DOMAIN_NAME'",
+                "Type": "A",
+                "TTL": 300,
+                "ResourceRecords": [{ "Value": "'$IP_ADDRESS'" }]
+            }
+        }]
+    }'
 done
